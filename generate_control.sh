e931203baeb41c5192c694bc6f433f6dad25236d
Dominik Meyer <dmeyer@federationhq.de>
2017-06-01 22:02:14 +0200

    ADD: added small helper script to generate control file

---
+++ generate_control.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+CONTROL_FILE="controls-byterazor-fhem-modules.txt"
+
+if [ -e $CONTROL_FILE ]; then
+  rm $CONTROL_FILE
+fi
+
+find ./FHEM -type f \( ! -iname "0.*" \) -print0 | while IFS= read -r -d '' f;
+do
+        out="UPD `stat --format "%z %s" $f | sed -e "s#\([0-9-]*\)\ \([0-9:]*\)\.[0-9]*\ [+0-9]*#\1_\2#"` $f"
+        echo ${out//.\//} >> $CONTROL_FILE
+done
